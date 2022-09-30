c7a583b03e2d75101a26d865a1133a994d0fcf24
Sam Stevens <samuel.robert.stevens@gmail.com>
2022-09-30 10:29:43 -0400

    boostrap packer

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,3 +1,17 @@
+local function ensurePacker()
+  local installPath = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+  if vim.fn.empty(vim.fn.glob(installPath)) > 0 then
+    vim.fn.system{'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', installPath}
+    vim.cmd 'packadd packer.nvim'
+    return true
+  end
+
+  return false
+end
+
+local packerBoostrap = ensurePacker()
+
 return require('packer').startup(function(use) 
   use 'wbthomason/packer.nvim'
 
@@ -9,4 +23,8 @@ return require('packer').startup(function(use)
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use 'f-person/git-blame.nvim'
   use 'dag/vim-fish'
+
+  if packerBootsrap then
+    require('packer').sync()
+  end
 end)
